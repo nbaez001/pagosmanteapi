7b48ca9bdefd58dec51899572cde20c8fe62fbba
Your Name <you@example.com>
2023-03-24 18:02:20 -0500

    Adiciona de Handler para solicitudes no autorizadas

---
+++ src/main/java/pe/gob/essalud/pagosmanteapi/config/AuthExceptionHandler.java
@@ -0,0 +1,18 @@
+package pe.gob.essalud.pagosmanteapi.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AuthExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<Object> handleAuthenticationException(AuthenticationException ex) {
+        // Manejar la excepción y redirigir a la página de inicio de sesión
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}
